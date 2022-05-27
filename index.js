0ea1b076119431cfe26eb943f5bd2dbc32d86ccd
Saiyadul77 <saiyadul77@gmail.com>
2022-05-27 23:36:58 +0600

    environment variable updated

--- index.js
+++ index.js
@@ -1,34 +1,96 @@
 const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors')
 
 
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: 'unauthorized access' });
+    }
+  
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      console.log('decoded', decoded);
+      req.decoded = decoded;
+      next();
+    })
+    // console.log('inside verifier', authHeader);
+    
+  }
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://dbUser5:kRoyp5QXyHvelqhu@cluster0.s3wad.mongodb.net/?retryWrites=true&w=majority";
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s3wad.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 async function run() {
     try {
         await client.connect();
-        const collection = client.db("computerParts").collection("accessories");
+        const serviceCollection = client.db("computerParts").collection("accessories");
 
-        app.get('/service', async(req, res)=>{
-            const query={};
-            const cursor= collection.find(query);
-            const services= await cursor.toArray();
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+        // multiple data get
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
             res.send(services);
         })
+        // single data get
+        app.get('/service/:id',  async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
+        // POST USER
+        app.post('/service', async (req, res) => {
+            const newUser = req.body;
+            console.log('adding new user', newUser);
+            const result = await serviceCollection.insertOne(newUser);
+            res.send(result)
+        })
+
+        // Delete 
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // Data decrease and update
+
+        
+
     }
     finally {
 
     }
 }
+
 
 run().catch(console.dir);
 
@@ -38,5 +100,5 @@ app.get('/', (req, res) => {
     res.send('Utkorsho Academic');
 })
 app.listen(port, () => {
-    console.log('Saiyadul amin', port);
+    console.log('Akhand Computer parts running', port);
 })
