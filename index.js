d12ac6c03ae6cc0d7fe67572adc7ff2e4827144d
Saiyadul77 <saiyadul77@gmail.com>
2022-05-29 17:48:39 +0600

    updated

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("computerParts").collection("accessories");
+        const bookingCollection = client.db("computerParts").collection("bookings");
+        const userCollection = client.db('computerParts').collection('users');
 
         app.post('/login', async (req, res) => {
             const user = req.body;
@@ -57,6 +59,20 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         })
+
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+          });
+
+        app.get('/available', async(req,res)=>{
+            const order =req.query.order;
+            const services= await serviceCollection.find().toArray();
+            const query= {order: order};
+            const bookings= await bookingCollection.find().toArray();
+            res.send(bookings)
+        })
+
         // single data get
         app.get('/service/:id',  async (req, res) => {
             const id = req.params.id;
@@ -65,13 +81,53 @@ async function run() {
             res.send(service);
         })
 
+        app.get('/booking', async(req, res) =>{
+            const customerEmail = req.query.customerEmail;
+            const query = {customerEmail: customerEmail};
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+          })
+
+          // PUT 
+          app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+              $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+          })
+
+          // create admin using put
+          app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+              const filter = { email: email };
+              const updateDoc = {
+                $set: { role: 'admin' },
+              };
+              const result = await userCollection.updateOne(filter, updateDoc);
+              res.send(result);
+            }
+            else{
+              res.status(403).send({message: 'forbidden'});
+            }
+      
+          })
+
         // POST USER
-        app.post('/service', async (req, res) => {
-            const newUser = req.body;
-            console.log('adding new user', newUser);
-            const result = await serviceCollection.insertOne(newUser);
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
             res.send(result)
         })
+
 
         // Delete 
         app.delete('/service/:id', async (req, res) => {
